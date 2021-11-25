7748ccda32dc93c223c03b75317ab6bf3bc3bbb4
Steven Oyer <contact.stevenoyer@gmail.com>
2021-11-25 16:16:16 +0100

    Debug

--- script.php
+++ script.php
@@ -8,16 +8,24 @@ $bdd = new Database('localhost', 'widget', 'widget', '1$9Zyjk0');
 
 
 $name = 'widget';
-$secret = '1$9Zyjk0';
+//$secret = '1$9Zyjk0';
+
+$sql_parts = [];
+$attributes = [];
 
 foreach ($_GET as $k => $v)
 {
     $get[$k] = htmlspecialchars($v);
+    $sql_parts[] = "$key = ?";
+    $attributes[] = htmlspecialchars($v);
 }
+$sql = implode(', ', $sql_parts);
 
 extract($get);
 
-$data = $bdd->save('INSERT INTO form_dev (prenom, token, client_id)', [$prenom, $secret, $client_id], true);
+
+
+$data = $bdd->save("INSERT INTO form_dev SET $sql", $attributes, true);
 
 var_dump($data);
 
